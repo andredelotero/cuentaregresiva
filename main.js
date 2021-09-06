8c13d916cdf94dc7ae2fd613dcbc3e0d90cf65c1
andre gama del otero <andredelotero@gmail.com>
2021-09-06 20:15:20 -0300

    agregado condicional para ingreso de numero entre 1 y 98

--- main.js
+++ main.js
@@ -1,32 +1,39 @@
+    let tiempoConsultado;
+    
+    function preguntar(){
+        tiempoConsultado = parseInt(prompt("ingresa la cantidad de minutos"));
+        if (!(tiempoConsultado>0 && tiempoConsultado<99)){
+            alert("tenes que ingrear un numero (que sea mayor que cero y menor que 99)");
+            preguntar();
+        }
+        return tiempoConsultado;
+    }
 
-    let tiempo = prompt("ingresa la cantidad de minutos");
-    let tiempoMinutos = tiempo;
+    let tiempoMinutos = preguntar();
     let tiempoSegundos= 0;
-function timer(){
+    
+    function timer(){
     tiempoMinutos= parseInt(tiempoMinutos);
     tiempoSegundos=parseInt(tiempoSegundos);
     if (tiempoSegundos>0){
-        tiempoSegundos--;
-      
+        tiempoSegundos--;      
     }    
     else{
         if (tiempoMinutos>0){           
             tiempoMinutos--;
             tiempoSegundos=59;
-      
         }
         else{
             if (tiempoMinutos==0 && tiempoSegundos==0){
                 document.querySelector(".puntos").classList.remove("titila");
             }
     }
-    
     }
     if (tiempoSegundos < 10 || tiempoSegundos == 0) {
-        tiempoSegundos= '0' + tiempoSegundos;
+        tiempoSegundos = '0' + tiempoSegundos;
       }
     if (tiempoMinutos < 10 || tiempoMinutos == 0) {
-        tiempoMinutos= '0' + tiempoMinutos;
+        tiempoMinutos = '0' + tiempoMinutos;
       }
 document.querySelector(".segundos").innerHTML=tiempoSegundos;
 document.querySelector(".minutos").innerHTML=tiempoMinutos;
@@ -34,11 +41,9 @@ document.querySelector(".minutos").innerHTML=tiempoMinutos;
 if ((parseInt(tiempoMinutos)==0) && (parseInt(tiempoSegundos)<20)){
     document.querySelector(".cuentaAtras").classList.add("seTermina");
     document.querySelector(".titulo").innerHTML="Se terminó el recreo!!";
-
 }
 }
 
 function dale(){
-
     setInterval(timer, 1000);
 }
